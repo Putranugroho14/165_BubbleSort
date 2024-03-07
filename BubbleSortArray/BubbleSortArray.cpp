0b5648f2ac65e447659512dbe9aa8537acfecde5
Putranugroho14 <putranugroho61@gmail.com>
2024-03-07 12:33:17 +0700

    Membuat prosedur array dengan metode bubblesort

--- BubbleSortArray/BubbleSortArray.cpp
+++ BubbleSortArray/BubbleSortArray.cpp
@@ -36,3 +36,17 @@ void display() {                                               // Prosedur untuk
 	}
 	cout << endl;                                        // Output Baris kosong
 }
+
+void BubbleSortArray() {                                // Prosedur untuk mengurutkan Array dengan metode bubblesort
+	int pass = 1;                                       // step 1
+
+	for (pass; pass <= n - 1; pass++) {               // looping dengan i dimulai dari 1 hingga n-1
+		for (int j = 0; j <= n - 1 - pass; j++) {       // looping dengan j dimulai dari 0 hingga n-1
+			if (a[j] > a[j + 1]) {                      // jika nilai pada a{j] lebih besar dari a[j + 1]
+				int temp = a[j];                        // simpan nilai a[j] ke variabel sementara temp
+				a[j] = a[j + 1];                        // Assign nilai a[j + 1] ke a [j]
+				a[j + 1] = temp;                        // Assign nilai temp ke a[j + 1]
+			}
+		}
+	}
+}
